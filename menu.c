b3cd7669c76fdefb02fd54ba469dd995133130be
apfreire1997 <152666365+apfreire1997@users.noreply.github.com>
2024-01-14 15:55:31 -0500

    menu.c

---
+++ menu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "menu.h"
+
+void mostrarMenu() {
+    printf("-------------------------------------------------------\n");
+    printf("|                         MENU                        |\n");
+    printf("-------------------------------------------------------\n");
+    printf("| 1. Ingresar Producto                                |\n");
+    printf("| 2. Editar Producto                                  |\n");
+    printf("| 3. Eliminar Producto                                |\n");
+    printf("| 4. Mostrar Inventario                               |\n");
+    printf("| 5. Ingresar Cliente                                  |\n");
+    printf("| 6. Editar Cliente                                    |\n");
+    printf("| 7. Mostrar Clientes                                  |\n");
+    printf("| 8. Facturar                                         |\n");
+    printf("| 9. Salir                                            |\n");
+    printf("-------------------------------------------------------\n");
+}
